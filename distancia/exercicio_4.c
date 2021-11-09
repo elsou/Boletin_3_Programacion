1bb317c00fd3dcb866e3c42671db40eec8206de2
Daniel Souto Pazó <45274010+elsou@users.noreply.github.com>
2021-11-09 01:56:14 +0100

    Update and rename main.c to exercicio_4.c

--- distancia/exercicio_4.c
+++ distancia/exercicio_4.c
@@ -1,9 +1,10 @@
 
-// 4. Distancia entre dias
+// 4. Días entre dúas datas
 
 #include <stdio.h>
 #include "dias.h"
 
+//Declaración de funcións
 Date ler();
 void mostrar (Date d, char formato);
 int calcular_distancia(Date d1, Date d2);
@@ -12,11 +13,13 @@ int vez = 1;
 char form;
 
 int main(){
-
+    
+    //O formato desexado gárdase na variable tipo char "f"
     printf("\nEste programa calcula a distancia entre duas datas.\n");
     printf("-En que formato prefire manexar as datas?\n (Para dd/mm/aaaa introduza 1)\n (Para mm/dd/aaaa introduza 2)\n");
     scanf("%c", &form);
-
+    
+    //O usuario introduce dúas datas, que se intercambian para adaptarse á orde cronolóxica e facilitar os cálculos
     Date dia1 = ler();
     Date dia2 = ler();
     if(dia1.year*10000 + dia1.month*100 + dia1.day > dia2.year*10000 + dia2.month*100 + dia2.day){
@@ -25,7 +28,8 @@ int main(){
         dia1 = dia2;
         dia2 = temp;
     }
-
+    
+    //Calcúlase e imprímese a diferenza de días entre as dúas datas introducidas
     int distancia = calcular_distancia(dia1, dia2);
     printf("A distancia entre o dia ");
     mostrar(dia1, form);
@@ -40,8 +44,10 @@ int main(){
 int calcular_distancia (Date d1, Date d2){
     int distancia;
     if(d1.year == d2.year){
+        //No mesmo ano réstanse os días que levan de ano
         distancia = days_so_far(d2) - days_so_far(d1);
     }else{
+        //En distintos anos, súmanse os días no último ano, os días que faltan para completar o primeiro ano, e os días dos anos intermedios
         distancia = days_so_far(d2) + days_to_come(d1) + days_in_years(d1.year, d2.year);
     }
     return distancia;
@@ -60,12 +66,13 @@ void mostrar(Date d, char formato){
 Date ler(){
     Date temp = {1, 1, 1};
     do{
+        //Isto imprímese se houbo algún dato incorrecto na anterior volta
         if(data_invalida(temp).day) printf("\nVolva intentalo (dia non valido)\n");
-        else if(data_invalida(temp).month) printf("\nVolva intentalo (mes non valido)\n"); //Isto imprímese se houbo algún dato incorrecto na anterior volta
+        else if(data_invalida(temp).month) printf("\nVolva intentalo (mes non valido)\n"); 
         else if(data_invalida(temp).year) printf("\nVolva intentalo (ano non valido):\n");
         else{
+            //Pide a data no formato elexido polo usuario
             printf("Escriba a %d data en formato ", vez);
-
             if(form == '1'){
                 printf("dd/mm/aaaa:\n");
             }else{
@@ -73,6 +80,7 @@ Date ler(){
             }
             vez += 1;
         }
+        //Le a data no formato elexido polo usuario
         if(form == '1'){
             scanf("%d/%d/%d", &temp.day, &temp.month, &temp.year);
         }else{
