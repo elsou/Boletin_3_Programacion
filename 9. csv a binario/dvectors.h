0fb96ec41e805b153e846921dc4f9a071512328d
Daniel Souto Pazó <45274010+elsou@users.noreply.github.com>
2021-11-09 18:03:47 +0100

    Update dvectors.h

--- 9. csv a binario/dvectors.h
+++ 9. csv a binario/dvectors.h
@@ -2,15 +2,15 @@
 
 #include <stdlib.h>
 
-//A estrutura dvec contén o vector e o seu tamaño para facilitar o seu manexo
+//A estrutura dvec (Dynamic Vector) garda o tamaño do propio vector dinámico para facilitar o seu manexo
 typedef struct dvec {
-    int *vector;
-    int size;
+  WeatherData *vector;
+  int size;
 } dvec;
 
 //Inicializa un vector dinámico
 dvec new_dvec() {
-    dvec temp = {(int*) malloc(sizeof(int)), 1};
+    dvec temp = {(WeatherData*) malloc(sizeof(WeatherData)), 1};
     return temp;
 }
 
@@ -18,15 +18,6 @@ dvec new_dvec() {
 dvec increase_size(dvec dvector, int n) {
     dvec temp = new_dvec();
     temp.size = dvector.size + n;
-    temp.vector = realloc(dvector.vector, sizeof(int)*(temp.size));
+    temp.vector = realloc(dvector.vector, sizeof(WeatherData)*(temp.size));
     return temp;
-}
-
-//Imprime un vector de enteiros
-void print_vector(dvec dvector) {
-    printf("(%i", dvector.vector[0]);
-    for(int i = 1; i < dvector.size; i++){
-        printf(", %i", dvector.vector[i]);
-    }
-    printf(")");
 }
