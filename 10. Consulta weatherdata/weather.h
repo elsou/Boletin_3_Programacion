9c0f3cb85f8fec6bad022b4105247969013ece9b
Daniel Souto Pazó <45274010+elsou@users.noreply.github.com>
2021-11-09 18:11:30 +0100

    Update weather.h

--- 10. Consulta weatherdata/weather.h
+++ 10. Consulta weatherdata/weather.h
@@ -1,13 +1,13 @@
 //weather.h
 
-typedef struct Date{
+typedef struct Date {
     int day;
     int month;
     int year;
 } Date;
 
 
-typedef struct WeatherData{
+typedef struct WeatherData {
     Date date;
     float longitude;
     float latitude;
@@ -21,7 +21,7 @@ typedef struct WeatherData{
 } WeatherData;
 
 //Imprime os datos tipo WeatherData
-void print_weatherdata(WeatherData wdata){
+void print_weatherdata(WeatherData wdata) {
     printf("\n{\n   Date (mm/dd/yyyy): %02d/%02d/%04d\n   Longitude: %f\n   Latitude: %f", wdata.date.month, wdata.date.day, wdata.date.year, wdata.longitude, wdata.latitude);
     printf("\n   Elevation: %d\n   Max. temperature: %f\n   Min. temperature: %f", wdata.elevation, wdata.max_temp, wdata.min_temp);
     printf("\n   Precipitation: %f\n   Wind: %f\n   Relative humidity: %f\n   Solar: %f\n}\n", wdata.precipitation, wdata.wind, wdata.rel_humidity, wdata.solar);
